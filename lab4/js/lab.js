1b314bc0d8924e056331ec1d2a00b33d5fbf8906
Andrew <dkim183@ucsc.edu>
2022-10-12 18:48:56 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+// Summary: Creating a website.
+// Description: Pseudo-coding for creating a basic website.
+// @link   URL
+// @file   lab.js
+// @author Huy Nguyen and Andrew Cho.
+// @since  10.12.2922
+
+//  Creating a webpage:
+
+//  Create a text document
+//  Edit the text file with a text editor.
+//  Type “<!DOCTYPE html> at the top of the document
+//  Underneath the previous step type “<html> </html>”.
+    //  Put your cursor in between the opening and closing tag and hit return/enter key on your keyboard to make the closing tag go below the opening tag.
+// In between the html tags type “<body> </body>” .
+    // Put your cursor in between the opening and closing tag and hit return/enter key on your keyboard to make the closing tag go below the opening tag.
+//  In between the body tags type “<p> </p>”.
+    // In between the p tags type whatever you want.
